8644016abb885b23e28cb2b5e45c3644b71c94b5
soualexandre <alexandreifto2@gmail.com>
2021-07-20 13:26:55 -0300

    melhorando

--- app/Http/Controllers/api/OrderController.php
+++ app/Http/Controllers/api/OrderController.php
@@ -17,7 +17,7 @@ class OrderController extends Controller
     public function index()
     {
         $budget = Budget::all()->toJson();
-        return $budget;
+        dd($budget);
     }
 
     /**
